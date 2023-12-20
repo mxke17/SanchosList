37e9a5c144c47555dd60e2b1ee68b82a0a6bfb99
thicco-catto <cristobal.martin01@gmail.com>
2023-12-20 11:17:31 +0100

    feat: Added select of all lists in main

--- SanchosLists/app/src/main/java/com/example/sanchoslists/MainActivity.java
+++ SanchosLists/app/src/main/java/com/example/sanchoslists/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.sanchoslists.tablecontracts.ListsContract;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         //listLists = (RecyclerView) findViewById(R.id.taskListRv);
         textView = (TextView) findViewById(R.id.textView2);
         initLists();
+
+        Cursor cursor = db.query(ListsContract.DictEntry.TABLE_NAME, null, null, null, null, null, null);
+        List<String> listNames = new ArrayList<>();
+
+        if(cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndex(ListsContract.DictEntry.COLUMN_NAME_NAME);
+            if (columnIndex > 0) {
+                do {
+                    listNames.add(cursor.getString(columnIndex));
+                } while (cursor.moveToNext());
+            }
+        }
+
+        //listNames tiene los nombres de todas las listas
     }
 
     private void initLists(){
