cdb010d8244701066935ccfe6845ab9c4759d120
thicco-catto <cristobal.martin01@gmail.com>
2023-12-20 10:25:09 +0100

    feat: Added table contracts

--- SanchosLists/app/src/main/java/com/example/sanchoslists/SanchosSQLite.java
+++ SanchosLists/app/src/main/java/com/example/sanchoslists/SanchosSQLite.java
@@ -2,21 +2,16 @@ package com.example.sanchoslists;
 import android.content.Context;
 import android.database.sqlite.*;
 
-import androidx.annotation.Nullable;
+import com.example.sanchoslists.tablecontracts.ListsContract;
+import com.example.sanchoslists.tablecontracts.TasksContract;
+
+import java.util.List;
 
 public class SanchosSQLite extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "sanchos_db";
     public static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_LISTS = "lists";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_LIST_NAME = "listName";
-
-    public static final String TABLE_TASKS = "tasks";
-    public static final String COLUMN_TASK_ID = "taskId";
-    public static final String COLUMN_TASK_NAME = "taskName";
-    public static final String COLUMN_LIST_ID = "listId";
     String SQL_DELETE_ENTRIES =  "DROP TABLE IF EXISTS " + DATABASE_NAME;
 
     public SanchosSQLite(Context context) {
@@ -26,17 +21,17 @@ public class SanchosSQLite extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         // Crear tabla de listas
-        String createListsTable = "CREATE TABLE " + TABLE_LISTS +
-                "(" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
-                COLUMN_LIST_NAME + " TEXT UNIQUE)";
+        String createListsTable = "CREATE TABLE " + ListsContract.DictEntry.TABLE_NAME +
+                "(" + ListsContract.DictEntry._ID + " INTEGER PRIMARY KEY, " +
+                ListsContract.DictEntry.COLUMN_NAME_NAME + " TEXT UNIQUE)";
         sqLiteDatabase.execSQL(createListsTable);
 
         // Crear tabla de tareas
-        String createTasksTable = "CREATE TABLE " + TABLE_TASKS +
-                "(" + COLUMN_TASK_ID + " INTEGER PRIMARY KEY, " +
-                COLUMN_TASK_NAME + " TEXT, " +
-                COLUMN_LIST_ID + " INTEGER, " +
-                "FOREIGN KEY(" + COLUMN_LIST_ID + ") REFERENCES " + TABLE_LISTS + "(" + COLUMN_ID + "))";
+        String createTasksTable = "CREATE TABLE " + TasksContract.DictEntry.TABLE_NAME +
+                "(" + TasksContract.DictEntry._ID + " INTEGER PRIMARY KEY, " +
+                TasksContract.DictEntry.COLUMN_NAME_NAME + " TEXT, " +
+                TasksContract.DictEntry.COLUMN_LIST_ID_NAME + " INTEGER, " +
+                "FOREIGN KEY(" + TasksContract.DictEntry.COLUMN_LIST_ID_NAME + ") REFERENCES " + ListsContract.DictEntry.TABLE_NAME + "(" + ListsContract.DictEntry._ID + "))";
         sqLiteDatabase.execSQL(createTasksTable);
     }
 
